b641a64939999057cf31bde025ac59e162542c87
Ilya Shandulskiy <ilyashandulskiy@gmail.com>
2023-06-13 19:37:13 +0500

    init

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,9 @@
+describe('the only test', () => {
+  it('should open page', () => {
+    cy.visit('http://localhost:3000')
+    const field = cy.get('input');
+    field.type('ilya')
+    cy.get('button').click();
+    cy.contains('Your name is ilya')
+  })
+})
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+export const App = () => {
+  const [name, setName] = useState('')
+  const [result, setResult] = useState('')
+
+  const onSubmit = () => {
+    setResult(name)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder={"Your name"} onInput={e => setName(e.currentTarget.value)}/>
+      <button onClick={onSubmit}>Show my name</button>
+
+      {!!result && <p>Your name is {result}</p>}
     </div>
   );
-}
-
-export default App;
+};
