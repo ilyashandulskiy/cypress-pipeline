e3616c0f6b573ed3028a253debe87495c01b292d
Ilya Shandulskiy <ilyashandulskiy@gmail.com>
2023-06-13 19:57:28 +0500

    break all

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,7 +2,7 @@ describe('the only test', () => {
   it('should open page', () => {
     cy.visit('http://localhost:3000')
     const field = cy.get('input');
-    field.type('ilya')
+    field.type('iiiiii')
     cy.get('button').click();
     cy.contains('Your name is ilya')
   })
